b9eb0d4c75a43542258e8057d47cfe127ebcfafd
jacosro <yavoy122@gmail.com>
2017-05-20 01:22:34 +0200

    Implemented Firebase login

--- app/src/main/java/dds/project/meet/presentation/LoginActivity.java
+++ app/src/main/java/dds/project/meet/presentation/LoginActivity.java
@@ -2,10 +2,18 @@ package dds.project.meet.presentation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import dds.project.meet.R;
 
@@ -14,36 +22,107 @@ import dds.project.meet.R;
  * Created by RaulCoroban on 24/04/2017.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
+
+    private static final String TAG = LoginActivity.class.toString();
+
+    public static final boolean AUTOMATIC_LOGIN = true; // If you don't want to login
+    public static final String DEFAULT_EMAIL = "dds@project.com";
+    public static final String DEFAULT_PASSWORD = "ddsproject";
+
+    public FirebaseAuth mFirebaseAuth;
+    public EditText mEmailEditText;
+    public EditText mPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        if (mFirebaseAuth.getCurrentUser() != null) {
+            Log.d(TAG, "User is logged in! Launching MainActivity");
+            loginCompleted(true);
+        }
+        Log.d(TAG, "User is not logged in");
+
         setContentView(R.layout.login);
+        mEmailEditText = (EditText) findViewById(R.id.usernameEditText);
+        mPasswordEditText = (EditText) findViewById(R.id.passwordEditText);
         Button login = (Button) findViewById(R.id.loginButton);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), MainActivity.class);
-                startActivity(intent);
+                if (AUTOMATIC_LOGIN) {
+                    mEmailEditText.getText().clear();
+                    mPasswordEditText.getText().clear();
+                    mEmailEditText.getText().append(DEFAULT_EMAIL);
+                    mPasswordEditText.getText().append(DEFAULT_PASSWORD);
+                }
+                checkLogin();
             }
         });
+    }
 
-        final EditText user = (EditText) findViewById(R.id.usernameEditText);
-        user.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                user.setText("");
-            }
-        });
+    private void checkLogin() {
+        showProgressDialog();
+        mEmailEditText.setError(null);
+        mPasswordEditText.setError(null);
 
-        final EditText pass = (EditText) findViewById(R.id.passwordEditText);
-        pass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pass.setText("");
-            }
-        });
+        boolean cancel = false;
+        View failed = null;
 
+        String email = mEmailEditText.getText().toString();
+        String password = mPasswordEditText.getText().toString();
+
+        if (!isPasswordOK(password)) {
+            cancel = true;
+            mPasswordEditText.setError("Password must be longer");
+            failed = mPasswordEditText;
+        }
+
+        if (!isEmailOK(email)) {
+            cancel = true;
+            mEmailEditText.setError("Email is not valid");
+            failed = mEmailEditText;
+        }
+
+        if (cancel) {
+            hideProgressDialog();
+            failed.requestFocus();
+        } else {
+            mFirebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()) {
+                        loginCompleted(true);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Incorrect email or password, please try again", Toast.LENGTH_SHORT).show();
+                        mEmailEditText.getText().clear();
+                        mPasswordEditText.getText().clear();
+                        hideProgressDialog();
+                    }
+
+                }
+            });
+        }
+    }
+
+    private boolean isEmailOK(String email) {
+        return email.matches("[^\\s]+@[^\\s]+\\.\\w+");
+    }
+
+    private boolean isPasswordOK(String password) {
+        return password.length() > 4;
+    }
+
+    public void loginCompleted(boolean finish) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        if (finish) {
+            finish();
+        }
     }
 }
