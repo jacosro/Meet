77c4d2581a038ee987ef258f87ab65c4d4c58278
jacosro <yavoy122@gmail.com>
2017-08-29 02:13:01 +0200

    Temporal fix on Google's Geocoder api missing

--- app/src/main/java/dds/project/meet/logic/util/GLocation.java
+++ app/src/main/java/dds/project/meet/logic/util/GLocation.java
@@ -52,25 +52,16 @@ public class GLocation {
         return mLocation;
     }
 
-    public LatLng getLatLng(String address) {
-        LatLng res = null;
-        try {
-            
-            Geocoder gc = new Geocoder(context);
-            List<Address> list = gc.getFromLocationName(address, 1);
-            if (!list.isEmpty()) {
-                Address add = list.get(0);
-                double latitude = add.getLatitude();
-                double longitude = add.getLongitude();
-                res = new LatLng(latitude, longitude);
-            }
-            return res;
-        } catch (IOException e) {
-            String apiAdd = address.replace(" ", "+");
-            // Use google api
-            // https://maps.googleapis.com/maps/api/geocode/json?address=apiAdd
-            return res;
+    public LatLng getLatLng(String address) throws IOException {
+        Geocoder gc = new Geocoder(context);
+        List<Address> list = gc.getFromLocationName(address, 1);
+        if (!list.isEmpty()) {
+            Address add = list.get(0);
+            double latitude = add.getLatitude();
+            double longitude = add.getLongitude();
+            return new LatLng(latitude, longitude);
         }
+        return null;
     }
 
     public boolean googleServicesOK() {
--- app/src/main/java/dds/project/meet/presentation/EventActivity.java
+++ app/src/main/java/dds/project/meet/presentation/EventActivity.java
@@ -16,6 +16,8 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -197,31 +199,33 @@ public class EventActivity extends BaseActivity implements OnMapReadyCallback, G
 
                     try {
                         mLocation = new GLocation(EventActivity.this, mEvent.getLocation(), EventActivity.this);
+
+                        if (googleMap == null) {
+                            googleMap = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+
+                            if (mLocation.googleServicesOK()) {
+                                Log.d("MAP_READY", "Enterning...");
+                                googleMap.getMapAsync(EventActivity.this);
+                                Log.d("MAP_READY", "InitMap");
+                            } else {
+                                Toast.makeText(EventActivity.this, "No map available", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            googleMap.getMapAsync(new OnMapReadyCallback() {
+                                @Override
+                                public void onMapReady(GoogleMap googleMap) {
+                                    googleMap.addMarker(new MarkerOptions().position(mLocation.getLocation())
+                                            .title("Marker on Event Place"));
+                                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLocation.getLocation(), 15));
+                                    refreshDistances();
+                                }
+                            });
+                        }
                     } catch (IOException e) {
                         Log.e(TAG, "Error on location: " + e);
+                        Toast.makeText(EventActivity.this, "Cannot get location information", Toast.LENGTH_SHORT).show();
                     }
 
-                    if (googleMap == null) {
-                        googleMap = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
-
-                        if (mLocation.googleServicesOK()) {
-                            Log.d("MAP_READY", "Enterning...");
-                            googleMap.getMapAsync(EventActivity.this);
-                            Log.d("MAP_READY", "InitMap");
-                        } else {
-                            Toast.makeText(EventActivity.this, "No map available", Toast.LENGTH_LONG).show();
-                        }
-                    } else {
-                        googleMap.getMapAsync(new OnMapReadyCallback() {
-                            @Override
-                            public void onMapReady(GoogleMap googleMap) {
-                                googleMap.addMarker(new MarkerOptions().position(mLocation.getLocation())
-                                        .title("Marker on Event Place"));
-                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLocation.getLocation(), 15));
-                                refreshDistances();
-                            }
-                        });
-                    }
                 } else {
                     Toast.makeText(EventActivity.this, "There was an error gathering event data", Toast.LENGTH_SHORT).show();
                     finish();
